396e4b15c1173a239f239df74140dc43b4187c5e
wawanneutron <wawanneutron1331@gmail.com>
2021-02-18 19:57:58 +0700

    update Bookcontroller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Book;
-use App\Http\Resources\BookColection;
+use App\Http\Resources\BooksColection;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function index()
     {
-        $books = new BookColection(Book::all()); //bisa all() / get ()
+        $books = new BooksColection(Book::all()); //bisa all() / get ()
         return $books;
     }
 
