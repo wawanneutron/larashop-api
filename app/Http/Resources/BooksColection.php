c47acf2bb6e65640cb26d3887f1c44552206f6a8
wawanneutron <wawanneutron1331@gmail.com>
2021-02-18 19:56:23 +0700

    update bookcolection

--- app/Http/Resources/BooksColection.php
+++ app/Http/Resources/BooksColection.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class BookColection extends JsonResource
+class BooksColection extends JsonResource
 {
     /**
      * Transform the resource into an array.
